670a10ba5c3d05b724a67c8afc33a75e6eaefbc4
Ken Mitton <mittonk@alum.mit.edu>
2026-02-07 17:33:24 -0500

    VNC and i3

--- raspi-packages.sh
+++ raspi-packages.sh
@@ -3,9 +3,19 @@
 sudo apt install tmux vim mosh dnsutils elinks gh
 
 # Sway
-sudo apt install sway waybar fonts-font-awesome sway-backgrounds
+sudo apt install sway waybar fonts-font-awesome sway-backgrounds dmenu
+
+# i3
+sudo apt install i3 rofi
+
+# VNC
+sudo apt install realvnc-vnc-server vncserver-virtual
 
 # opensimh dependencies
 sudo apt install libsdl2-ttf-dev libsdl2-dev libpng-dev libpcap-dev libvdeplug-dev vde2
 
+# RetroPie dependencies
+sudo apt install libfreeimage-dev
+
+# Misc
 #sudo apt install irssi
